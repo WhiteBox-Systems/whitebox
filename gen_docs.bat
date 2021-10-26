c5b21b19c72b0f76eee8b07c2f033b06eddcd695
Andrew Reece <azmreece@gmail.com>
2021-10-26 17:56:33 +0100

    Fix version generation when created via Meson

--- gen_docs.bat
+++ gen_docs.bat
@@ -1,6 +1,8 @@
 @echo off
 set root=%MESON_SOURCE_ROOT%
 if "%root%" == "" (set root=.)
-git describe --abbrev=0 > docs\whitebox_docs\VERSION
-asciidoctor --warnings --require asciidoctor-pdf --require rouge --backend pdf   %root%\docs\whitebox_docs\readme.adoc
-asciidoctor --warnings --require asciidoctor-pdf --require rouge --backend html5 %root%\docs\whitebox_docs\readme.adoc
+set docs=%root%\docs\whitebox_docs
+
+git describe --abbrev=0 > %docs%\VERSION
+asciidoctor --warnings --require asciidoctor-pdf --require rouge --backend pdf   %docs%\readme.adoc
+asciidoctor --warnings --require asciidoctor-pdf --require rouge --backend html5 %docs%\readme.adoc
