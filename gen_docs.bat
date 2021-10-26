c35d62de8c83b756cc16371ecb9beb6f2bfbce31
Andrew Reece <azmreece@gmail.com>
2021-10-26 16:59:00 +0100

    Add handling for being called by Meson
    When Meson calls scripts, the working directory is unspecified

--- gen_docs.bat
+++ gen_docs.bat
@@ -1,4 +1,6 @@
 @echo off
+set root=%MESON_SOURCE_ROOT%
+if "%root%" == "" (set root=.)
 git describe --abbrev=0 > docs\whitebox_docs\VERSION
-asciidoctor --warnings --require asciidoctor-pdf --require rouge --backend pdf   docs\whitebox_docs\readme.adoc
-asciidoctor --warnings --require asciidoctor-pdf --require rouge --backend html5 docs\whitebox_docs\readme.adoc
+asciidoctor --warnings --require asciidoctor-pdf --require rouge --backend pdf   %root%\docs\whitebox_docs\readme.adoc
+asciidoctor --warnings --require asciidoctor-pdf --require rouge --backend html5 %root%\docs\whitebox_docs\readme.adoc
