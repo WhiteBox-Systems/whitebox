e3a84a14125a1029dea61fda59b39e1ff5f9bbf9
Andrew Reece <azmreece@gmail.com>
2022-02-23 13:20:54 +0000

    Fix script not building both PDF and HTML versions
    Script finishes after first execution if `call` isn't used

--- gen_docs.bat
+++ gen_docs.bat
@@ -4,5 +4,5 @@ if "%root%" == "" (set root=.)
 set docs=%root%\docs\whitebox_docs
 
 git describe --abbrev=0 > %docs%\VERSION
-asciidoctor --warnings --require asciidoctor-pdf --require rouge --backend pdf   %docs%\readme.adoc
-asciidoctor --warnings --require asciidoctor-pdf --require rouge --backend html5 %docs%\readme.adoc
+call asciidoctor --warnings --require asciidoctor-pdf --require rouge --backend pdf   %docs%\readme.adoc
+call asciidoctor --warnings --require asciidoctor-pdf --require rouge --backend html5 %docs%\readme.adoc
