422f0b64883e20895b1341a16527717dc41d27fe
Andrew Reece <azmreece@gmail.com>
2022-04-28 15:55:04 +0100

    Embed images in docs instead of linking to external files

--- gen_docs.bat
+++ gen_docs.bat
@@ -3,6 +3,8 @@ set root=%MESON_SOURCE_ROOT%
 if "%root%" == "" (set root=.)
 set docs=%root%\docs\whitebox_docs
 
+:: -a data-uri : embeds images in the document rather than linking to an external file
+
 git describe --abbrev=0 > %docs%\VERSION
-call asciidoctor --warnings --require asciidoctor-pdf --require rouge --backend pdf   %docs%\readme.adoc
-call asciidoctor --warnings --require asciidoctor-pdf --require rouge --backend html5 %docs%\readme.adoc
+call asciidoctor -a data-uri --warnings --require asciidoctor-pdf --require rouge --backend pdf   %docs%\readme.adoc
+call asciidoctor -a data-uri --warnings --require asciidoctor-pdf --require rouge --backend html5 %docs%\readme.adoc
