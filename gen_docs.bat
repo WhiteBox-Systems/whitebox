75490bf8ea04eb43c00d93a82e263c2d2acbf895
Andrew Reece <azmreece@gmail.com>
2022-05-13 15:04:02 +0100

    Default to WhiteBox root as opposed to `.`

--- gen_docs.bat
+++ gen_docs.bat
@@ -1,10 +1,10 @@
 @echo off
 set root=%MESON_SOURCE_ROOT%
-if "%root%" == "" (set root=.)
+if "%root%" == "" (set root=%~dp0\..\..)
 set docs=%root%\docs\whitebox_docs
 
 :: -a data-uri : embeds images in the document rather than linking to an external file
 
-git describe --abbrev=0 > %docs%\VERSION
-call asciidoctor -a data-uri --warnings --require asciidoctor-pdf --require rouge --backend pdf   %docs%\readme.adoc
-call asciidoctor -a data-uri --warnings --require asciidoctor-pdf --require rouge --backend html5 %docs%\readme.adoc
+git describe --abbrev=0 > "%docs%\VERSION"
+call asciidoctor -a data-uri --warnings --require asciidoctor-pdf --require rouge --backend pdf   "%docs%\readme.adoc"
+call asciidoctor -a data-uri --warnings --require asciidoctor-pdf --require rouge --backend html5 "%docs%\readme.adoc"
